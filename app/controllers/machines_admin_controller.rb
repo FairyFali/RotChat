f130d0a4b0b7e35e45238918b8b4d1ae664c82d4
wangfali <2529440267@qq.com>
2018-12-01 22:07:21 +0800

    增加机器人管理功能

---
+++ app/controllers/machines_admin_controller.rb
@@ -0,0 +1,80 @@
+class MachinesAdminController < ApplicationController
+	before_action :set_machine, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!  # 這個是 devise 提供的方法，先檢查必須登入
+	before_action :authenticate_admin # 再檢查是否有權限
+
+
+
+	# GET /machines
+	# GET /machines.json
+	def index
+		@machines = Machine.all
+	end
+
+	# GET /machines/1
+	# GET /machines/1.json
+	def show
+	end
+
+	# GET /machines/new
+	def new
+		@machine = Machine.new
+	end
+
+	# GET /machines/1/edit
+	def edit
+	end
+
+	# POST /machines
+	# POST /machines.json
+	def create
+		@machine = Machine.new(machine_params)
+		@machine.req_num = 0
+		@machine.duration = 0
+		respond_to do |format|
+		  if @machine.save
+		    format.html { redirect_to '/admin/machines/' + @machine.id.to_s, notice: 'machine was successfully created.' }
+		    format.json { render :show, status: :created, location: @machine }
+		  else
+		    format.html { render :new }
+		    format.json { render json: @machine.errors, status: :unprocessable_entity }
+		  end
+		end
+	end
+
+	# PATCH/PUT /machines/1
+	# PATCH/PUT /machines/1.json
+	def update
+		respond_to do |format|
+		  if @machine.update(machine_params)
+		    format.html { redirect_to '/admin/machines/'+@machine.id.to_s, notice: 'machine was successfully updated.' }
+		    format.json { render :show, status: :ok, location: @machine }
+		  else
+		    format.html { render :edit }
+		    format.json { render json: @machine.errors, status: :unprocessable_entity }
+		  end
+		end
+	end
+
+	# DELETE /machines/1
+	# DELETE /machines/1.json
+	def destroy
+		@machine.destroy
+		respond_to do |format|
+		  format.html { redirect_to '/admin/machines', notice: 'machine was successfully destroyed.' }
+		  format.json { head :no_content }
+		end
+	end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_machine
+      @machine = Machine.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white machine through.
+    def machine_params
+      params.require(:machine).permit(:api_key, :name, :url, :code)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,29 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   
+  # 聊天室
   resources :chats
+  # 机器人管理信息的URL
   resources :machines do
     collection do
       get :index_json
     end
   end
-  resources :machine_messages
-  post '/machineusers' => 'machines_users#add'
 
+  # 机器人回复信息的URL
+  resources :machine_messages
+  # 增加用户的机器人好友
+  post 'machineusers' => 'machines_users#add'
+  # 管理员机器人管理
+  get '/admin/machines', to: 'machines_admin#index'
+  post '/admin/machines', to: 'machines_admin#create'
+  get '/admin/machines/new', to: 'machines_admin#new'
+  get '/admin/machines/:id(.:format)', to: 'machines_admin#show'
+  put '/admin/machines', to: 'machines_admin#update'
+  patch '/admin/machines', to: 'machines_admin#update'
+  delete '/admin/machines/:id(.:format)', to: 'machines_admin#destroy'
+  get '/admin/machines/:id/edit(.:format)', to: 'machines_admin#edit'
+
+  # 根目录
   root 'home#index'
 end
